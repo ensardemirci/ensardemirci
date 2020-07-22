863a65bd81fba27554548ed2af6264f3f0e59a52
Ensar Demirci <59653298+ensardemirci@users.noreply.github.com>
2020-07-22 17:04:25 +0300

    Initial commit

---
+++ pages/urls.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+
+    path('', views.index, name='index'),
+    path('ozgecmis', views.about, name='about'),
+    path('blog', views.blog, name='blog'),
+    path('iletisim', views.contact, name='contact')
+
+]
