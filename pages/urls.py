def1596011d896166542e0a04f684a5ae7311fa6
Ensar Demirci <59653298+ensardemirci@users.noreply.github.com>
2020-07-23 11:20:55 +0300

    Contact Form

--- pages/urls.py
+++ pages/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('ozgecmis', views.about, name='about'),
     path('blog', views.blog, name='blog'),
-    path('iletisim', views.contact, name='contact')
+    path('iletisim', views.contact_us, name='contact')
 
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import ContactForm
+from django.core.mail import send_mail
 
 # Create your views here.
 
@@ -12,7 +14,22 @@ def about(request):
 def blog(request):
     return render(request, 'pages/blog.html')
 
-def contact(request):
-    return render(request, 'pages/contact.html')
+# def contact(request):
+#     return render(request, 'pages/contact.html')
+
+def contact_us(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            sender_name = form.cleaned_data['name']
+            sender_email = form.cleaned_data['email']
+
+            message = f'İsim : {sender_name} Mesaj : {form.cleaned_data["message"]}'
+            send_mail('ed.com Yeni Mesaj', message, sender_email, ['ensardemirci93@gmail.com'])
+            return HttpResponse('Teşekkürler Mesajınız Alındı')
+    else:
+        form = ContactForm()
+    return render(request,'pages/contact.html',{'form': form})
+
 
 
